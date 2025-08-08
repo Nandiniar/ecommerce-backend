0d83bc66290da19847d0d2ac20c76f723730f34f
nandini <nandani@contaque.com>
2025-08-08 11:32:38 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/main/java/com/ecommerce/ecommerce/springproject/config/MyDataRestConfig.java
+++ src/main/java/com/ecommerce/ecommerce/springproject/config/MyDataRestConfig.java
@@ -2,18 +2,32 @@ package com.ecommerce.ecommerce.springproject.config;
 
 import com.ecommerce.ecommerce.springproject.Entity.Product;
 import com.ecommerce.ecommerce.springproject.Entity.Product_category;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig  implements RepositoryRestConfigurer { //here repositoryRestConfigurer means ke rest api endpoint me kaunse method ko hum able ya disable kar sakte hao
     // is repositoryRestConfigurer me  hm cors etc bhi integrate kar sakte hai
 
+    private EntityManager entityManager; // autowire jpa entity manager
+   // EntityManager: A JPA (Java Persistence API) tool to interact with the database and manage entities.
 
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager){
+        entityManager=theEntityManager;
+    }
+
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions={HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE}; // meand disable http method for Put,Post,Delete
 
 
@@ -27,5 +41,23 @@ public class MyDataRestConfig  implements RepositoryRestConfigurer { //here repo
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure(((metdata, httpMethods) ->httpMethods.disable(theUnsupportedActions) ));
 
+        // call an internal helper method
+        exposeIds(config);
+    }
+    private void exposeIds(RepositoryRestConfiguration config){
+
+        //expose entity ids
+        // get a list of all entity classes from entity manager
+        Set<EntityType<?>>entities=entityManager.getMetamodel().getEntities();
+        // create an array of the entity type
+        List<Class> entityClasses=new ArrayList<>();
+
+        // get the entity types for the entities
+        for(EntityType tempEntityType:entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+        // expose the entitiy ids for the array of entity/domain type
+        Class[] domainType=entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainType);
     }
 }
--- src/main/java/com/ecommerce/ecommerce/springproject/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/ecommerce/springproject/dao/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.ecommerce.ecommerce.springproject.dao;
 
 import com.ecommerce.ecommerce.springproject.Entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,4 +12,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(collectionResourceRel = "product", path="product")
 public interface ProductRepository extends JpaRepository<Product,Long> { // here Long means the datatype of Primary
     // key of  product is Long
+
+
+    // here findbyCategoryId is a query method which work similar to select * from product where id=? and in our page function
+    // id will be used as a parameter value
+    Page<Product>findByCategoryId(@Param("id") Long id, Pageable pageable);
+    // this line will  work similar  to select * from product where category_id=?
+
+    // page and pagable provides support for pagination
+    //page is a sublist of a list of objects and has information such as totalElements,totalPages,Currentposition etc
+    // pageable represent pagination information such as page number, page size,previous,next etc
+    // query method starts by findby,searchby and its endpoints are /search/<<queryMethodName
 }
